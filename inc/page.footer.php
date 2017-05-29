6d80ca93b4609b0a0e6ed13a66f2b70d1c7afe73
Phoenix Eve Aspacio <aspaciop@phoenixpeca.xyz>
2017-05-29 19:59:42 +0800

    Indent fix footer.php

--- inc/page.footer.php
+++ inc/page.footer.php
@@ -1,9 +1,9 @@
 <?php
 if(strtolower(basename($_SERVER["SCRIPT_FILENAME"])) === strtolower("page.footer.php")) {
-    setcookie('EM', '09', '0', '/');
-    header('Location: /');
-    //echo "Access Denied";
-  	exit;
+  setcookie('EM', '09', '0', '/');
+  header('Location: /');
+  //echo "Access Denied";
+	exit;
 }
 ?>    <footer class="footer">
       <div class="container">
