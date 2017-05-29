09fe83be524bcce4b1fe02d2e6ce59625a239e14
Phoenix Eve Aspacio <aspaciop@phoenixpeca.xyz>
2017-05-29 20:41:45 +0800

    New functions library

---
+++ inc/functions.php
@@ -0,0 +1,13 @@
+<?php
+function slugify($text) {
+  $text = preg_replace('~[^\pL\d]+~u', '-', $text);
+  $text = iconv('utf-8', 'us-ascii//TRANSLIT', $text);
+  $text = preg_replace('~[^-\w]+~', '', $text);
+  $text = trim($text, '-');
+  $text = preg_replace('~-+~', '-', $text);
+  $text = strtolower($text);
+  if (empty($text)) {
+    return 'n-a';
+  }
+  return $text;
+}
