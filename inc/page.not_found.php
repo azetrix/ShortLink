4bcd2452d604482689652ab7112e3f14f73e312b
Phoenix Eve Aspacio <aspaciop@phoenixpeca.xyz>
2017-05-29 20:00:01 +0800

    Indent fix page.not_found.php

--- inc/page.not_found.php
+++ inc/page.not_found.php
@@ -1,9 +1,9 @@
 <?php
 if(strtolower(basename($_SERVER["SCRIPT_FILENAME"])) === strtolower("page.not_found.php")) {
-    setcookie('EM', '09', '0', '/');
-    header('Location: /');
-    //echo "Access Denied";
-  	exit;
+  setcookie('EM', '09', '0', '/');
+  header('Location: /');
+  //echo "Access Denied";
+	exit;
 }
 include_once('./inc/page.header.php');
 ?>
