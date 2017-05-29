934b4e097160f9a2917c2e394594ff4967c1c525
Phoenix Eve Aspacio <aspaciop@phoenixpeca.xyz>
2017-05-29 20:42:13 +0800

    Footer text as constant

--- inc/vars.php
+++ inc/vars.php
@@ -17,6 +17,8 @@ define("SHORTLINK_CODE_CHARS", "123456789bcdfghjkmnpqrstvwxyzBCDFGHJKLMNPQRSTVWX
 
 define("FORWADER_PREVIEW", true); // enabling this feature prevents automatic redirection to malicious websites; toggling this feature requires clearing of cache
 
+define("FOOTER_TXT", 'Keith Joshua v0.1.0'); // footer text
+
 // Any ShortLink that matches with the keys of this array will be blocked. - ACCEPTS SHELL WILDCARD PATTERNS
 $kw_blacklist = array (
 '*--*',
