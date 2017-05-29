6fea64e5eda3c556fa9438a15814bace61c766a2
Phoenix Eve Aspacio <aspaciop@phoenixpeca.xyz>
2017-05-29 19:35:50 +0800

    Indent fix

--- forwarder.php
+++ forwarder.php
@@ -5,31 +5,31 @@ include('./inc/ShortLink.php');
 $forwardSC = ltrim(rtrim(preg_replace('~/+~', '/', trim(strtok($_SERVER['REQUEST_URI'], '?'))), '/'), '/');
 
 if($forwardSC == 'forwarder.php') {
-    setcookie('EM', '09', '0', '/');
-    header('Location: /');
-    //echo "Access Denied";
-  	exit;
+  setcookie('EM', '09', '0', '/');
+  header('Location: /');
+  //echo "Access Denied";
+	exit;
 }
 
 if(substr($forwardSC, -11) == '/index.html') {
-    $forwardSC = substr($forwardSC, 0, -11);
+  $forwardSC = substr($forwardSC, 0, -11);
 }
 if(substr($forwardSC, -10) == '/index.htm') {
-    $forwardSC = substr($forwardSC, 0, -10);
+  $forwardSC = substr($forwardSC, 0, -10);
 }
 if(substr($forwardSC, -10) == '/index.php') {
-    $forwardSC = substr($forwardSC, 0, -10);
+  $forwardSC = substr($forwardSC, 0, -10);
 }
 if(substr($forwardSC, -11) == '/index.aspx') {
-    $forwardSC = substr($forwardSC, 0, -11);
+  $forwardSC = substr($forwardSC, 0, -11);
 }
 if(substr($forwardSC, -10) == '/index.asp') {
-    $forwardSC = substr($forwardSC, 0, -10);
+  $forwardSC = substr($forwardSC, 0, -10);
 }
 
 try {
-    $pdo = new PDO(DB_PDODRIVER . ":host=" . DB_HOST . ";dbname=" . DB_DATABASE,
-        DB_USERNAME, DB_PASSWORD);
+  $pdo = new PDO(DB_PDODRIVER . ":host=" . DB_HOST . ";dbname=" . DB_DATABASE,
+    DB_USERNAME, DB_PASSWORD);
 }
 catch (\PDOException $e) {
   setcookie('EM', '14', '0', '/');
@@ -41,19 +41,18 @@ catch (\PDOException $e) {
 $shortLink = new ShortLink($pdo);
 
 try {
-    $url = $shortLink->shortCodeToUrl($forwardSC);
+  $url = $shortLink->shortCodeToUrl($forwardSC);
 }
 catch (\Exception $e) {
-    header($_SERVER["SERVER_PROTOCOL"]." 404 Not Found");
-    include_once('./inc/page.not_found.php');
-    exit;
-
+  header($_SERVER["SERVER_PROTOCOL"]." 404 Not Found");
+  include_once('./inc/page.not_found.php');
+  exit;
 }
 
 if(!FORWADER_PREVIEW) {
-    header($_SERVER["SERVER_PROTOCOL"]." 301 Moved Permanently");
-    header('Location: '.$url);
-  	exit;
+  header($_SERVER["SERVER_PROTOCOL"]." 301 Moved Permanently");
+  header('Location: '.$url);
+	exit;
 }
 
 include_once('./inc/page.header.php');
