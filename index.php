be62b795c157ba45f394161b29d859e55e07319f
Phoenix Eve Aspacio <aspaciop@phoenixpeca.xyz>
2017-05-29 20:23:37 +0800

    Fix broken link if URI is modified

--- index.php
+++ index.php
@@ -16,7 +16,7 @@ include_once('./inc/page.header.php');
 
       <div class="column">
         <div class="box">
-         <form action="create.php" method="post">
+         <form action="/create.php" method="post">
           <input readonly="true" style="display: none;" name="token" value="<?=bin2hex(openssl_random_pseudo_bytes(10));?>">
           <h2 class="title">Create a ShortLink</h2>
           <label title="Required" class="label">Enter URL <span style="color: red;">*</span></label>
