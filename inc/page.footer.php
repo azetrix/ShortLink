9d0496ff54092c26626c87f22f5c8d656d4d25e0
Phoenix Eve Aspacio <aspaciop@gmail.com>
2017-06-04 06:24:17 +0800

    Added Cookie Policy

--- inc/page.footer.php
+++ inc/page.footer.php
@@ -13,6 +13,7 @@ if(strtolower(basename($_SERVER["SCRIPT_FILENAME"])) === strtolower("page.footer
       </div>
     </footer>
     <script type="text/javascript" src="/assets/design.js<?='?'.slugify(FOOTER_TXT);?>"></script>
-    <script type="text/javascript" id="cookiebanner" src="/assets/cookiebanner.js<?='?'.slugify(FOOTER_TXT);?>"></script>
+    <script type="text/javascript" id="cookiebanner" src="/assets/cookiebanner.js<?='?'.slugify(FOOTER_TXT);?>"
+        data-message="We rely solely on cookies to give you service. By continuing to visit this site you agree to our use of cookies."></script>
   </body>
 </html>
